96211072bf0b509859827f3d650f8c890a8d9fa7
Quelich <emre1kilic@outlook.com>
2020-09-07 21:41:32 +0300

    Added Some Improvements

--- Converx/ASCII_Database.cs
+++ Converx/ASCII_Database.cs
@@ -1,43 +1,116 @@
 using System.Collections.Generic;
 using System;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
 
-
+//TODO Find a better way to compare the char to the string 
 namespace Converx
 {
-    public class ASCII_Database
+    public  class ASCII_Database
     {
-        public  Dictionary<string, string> DicASCII = new Dictionary<string, string>();
+        public static void Main(string[] args)
+        {
+            //GetInput();
+            ShowAll();
+        }
+
+
+        /// <summary>
+        /// Stores letter's ASCII code equivalent
+        /// </summary>
+        /// <typeparam name="string"> TYPE</typeparam>
+        /// <typeparam name="string">VALUE</typeparam>
+        /// <returns></returns>
+        public static Dictionary<string, string> DicBinary = new Dictionary<string, string>();
+        
+        public static void DatabaseASCII()
+        {
+            DicBinary.Add("a", "01100001");
+            DicBinary.Add("b", "01100010");
+            DicBinary.Add("c", "01100011");
+            DicBinary.Add("d", "01100100");
+            DicBinary.Add("e", "01100101");
+            DicBinary.Add("f", "01100110");
+            DicBinary.Add("g", "01100111");
+            DicBinary.Add("h", "01101000");
+            DicBinary.Add("i", "01101001");
+            DicBinary.Add("j", "01101010");
+            DicBinary.Add("k", "01101011");
+            DicBinary.Add("l", "01101100");
+            DicBinary.Add("m", "01101101");
+            DicBinary.Add("n", "01101110");
+            DicBinary.Add("o", "01101111");
+            DicBinary.Add("p", "01110000");
+            DicBinary.Add("q", "01110001");
+            DicBinary.Add("r", "01110010");
+            DicBinary.Add("s", "01110011");
+            DicBinary.Add("t", "01110100");
+            DicBinary.Add("u", "01110101");
+            DicBinary.Add("v", "01110110");
+            DicBinary.Add("w", "01110111");
+            DicBinary.Add("x", "01111000");
+            DicBinary.Add("y", "01111001");
+            DicBinary.Add("z", "01111010");
+
+        }
+        /// <summary>
+        /// Initializes all of Database
+        /// </summary>
+        public static void ShowAll()
+        {
+            foreach (KeyValuePair<string, string> kvm in DicBinary)
+            {
+                Console.WriteLine($"Key :{kvm.Key}: |Value :{kvm.Value}:");
+            }
+        }
         
 
-        public void DatabaseASCII()
+        public static void GetInput()
         {
-            DicASCII.Add("a", "097");
-            DicASCII.Add("b", "098");
-            DicASCII.Add("c", "099");
-            DicASCII.Add("d", "100");
-            DicASCII.Add("e", "101");
-            DicASCII.Add("f", "102");
-            DicASCII.Add("g", "103");
-            DicASCII.Add("h", "104");
-            DicASCII.Add("i", "105");
-            DicASCII.Add("j", "106");
-            DicASCII.Add("k", "107");
-            DicASCII.Add("l", "108");
-            DicASCII.Add("m", "109");
-            DicASCII.Add("n", "110");
-            DicASCII.Add("o", "111");
-            DicASCII.Add("p", "112");
-            DicASCII.Add("q", "113");
-            DicASCII.Add("r", "114");
-            DicASCII.Add("s", "115");
-            DicASCII.Add("t", "116");
-            DicASCII.Add("u", "117");
-            DicASCII.Add("v", "118");
-            DicASCII.Add("w", "119");
-            DicASCII.Add("x", "120");
-            DicASCII.Add("y", "121");
-            DicASCII.Add("z", "122");
 
+            Console.WriteLine("Write down something...");
+            
+            string InputWord = "emre";
+            Console.WriteLine($"Hello {InputWord}");
+
+            char[] MatchUp;
+            MatchUp = InputWord.ToCharArray(0,4);
+            foreach (var item in MatchUp)
+            {
+                foreach (var BinPair in DicBinary)
+                {
+                    if (item.Equals(BinPair.Key))
+                    {
+                        Console.Write(BinPair.Value);
+                    }  
+                }
+                switch (item)
+                {
+                    case 'a':
+                        Console.Write("01100001");
+                        break;
+                    case 'e':
+                        Console.Write("01100101");
+                        break;
+                        case 'm':
+                        Console.Write("01101101");
+                        break;
+                        case 'r':
+                        Console.Write("01110010");
+                        break;
+                    default:
+                        Console.Write("Invalid Input");
+                        break;
+                }
+            }
+        }
+
+        public static void Split(string token)
+        {
+            
         }
     }
 }
--- Converx/Program.cs
+++ Converx/Program.cs
@@ -1,24 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System.Data.Common;
+using System.Collections.Generic;
 using System;
 using Converx;
 
-
-
-
+//Get ASCII Codes
+//Justify their equivalents
+//Get The ınput from the user
+//Parse the each letter one ny one
+//Intialize the ASCII codes with respect to given letters
 namespace Converx
 {   
-    public class Program 
+    public class Program : ASCII_Database 
     {
-        
-        //Get ASCII Codes
-        //Justify their equivalents
-        //Get The ınput from the user
-        //Parse the each letter one ny one
-        //Intialize the ASCII codes with respect to given letters
-        public static void Main()
+        public void Trial()
         {
 
         }
         
+
+
     }
 }
