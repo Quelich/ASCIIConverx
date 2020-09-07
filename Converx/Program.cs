6a2d3812e6b54a867d78d66e52fc8b80c1833bff
Quelich <emre1kilic@outlook.com>
2020-09-07 14:03:46 +0300

    Initial commit

---
+++ Converx/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Converx
+{
+    public class Program
+    {
+        //Get ASCII Codes
+        //Justify their equivalents
+        //Get The ınput from the user
+        //Parse the each letter one ny one
+        //Intialize the ASCII codes with respect to given letters
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ASCIIConverx
+ ASCII Converter 
