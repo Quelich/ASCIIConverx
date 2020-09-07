0108e0cdd72dbded10578aa22e7bf7b482c19533
Quelich <emre1kilic@outlook.com>
2020-09-07 20:19:21 +0300

    Added ASCII_Database

---
+++ Converx/ASCII_Database.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System;
+
+
+namespace Converx
+{
+    public class ASCII_Database
+    {
+        public  Dictionary<string, string> DicASCII = new Dictionary<string, string>();
+        
+
+        public void DatabaseASCII()
+        {
+            DicASCII.Add("a", "097");
+            DicASCII.Add("b", "098");
+            DicASCII.Add("c", "099");
+            DicASCII.Add("d", "100");
+            DicASCII.Add("e", "101");
+            DicASCII.Add("f", "102");
+            DicASCII.Add("g", "103");
+            DicASCII.Add("h", "104");
+            DicASCII.Add("i", "105");
+            DicASCII.Add("j", "106");
+            DicASCII.Add("k", "107");
+            DicASCII.Add("l", "108");
+            DicASCII.Add("m", "109");
+            DicASCII.Add("n", "110");
+            DicASCII.Add("o", "111");
+            DicASCII.Add("p", "112");
+            DicASCII.Add("q", "113");
+            DicASCII.Add("r", "114");
+            DicASCII.Add("s", "115");
+            DicASCII.Add("t", "116");
+            DicASCII.Add("u", "117");
+            DicASCII.Add("v", "118");
+            DicASCII.Add("w", "119");
+            DicASCII.Add("x", "120");
+            DicASCII.Add("y", "121");
+            DicASCII.Add("z", "122");
+
+        }
+    }
+}
--- Converx/Program.cs
+++ Converx/Program.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
+using Converx;
+
+
+
 
 namespace Converx
-{
-    public class Program
+{   
+    public class Program 
     {
+        
         //Get ASCII Codes
         //Justify their equivalents
         //Get The ınput from the user
         //Parse the each letter one ny one
         //Intialize the ASCII codes with respect to given letters
+        public static void Main()
+        {
+
+        }
+        
     }
 }
